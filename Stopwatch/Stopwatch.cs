35b358b051b4ea0e2e539ed52231df1fe0f556db
maksimgornostaev <maksimgornostaev197@gmail.com>
2023-11-29 16:37:44 +0300

    Add code

---
+++ Stopwatch/Stopwatch.cs
@@ -0,0 +1,23 @@
+﻿namespace Stopwatch
+{
+    public partial class Program
+    {
+        public class Stopwatch
+        {
+            DateTime start;
+            DateTime stop;
+
+            public void Start()
+            {
+                start = DateTime.Now;
+            }
+
+            public void Stop()
+            {
+                stop = DateTime.Now;
+                TimeSpan time = stop - start;
+                Console.WriteLine("Differences in time: " + time);
+            }
+        }
+    }
+}
